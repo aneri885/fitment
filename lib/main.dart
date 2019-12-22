aae7305ba1e58a79d4e3369253d8260c44445845
Maurice Gerhardt <mrcgrhrdt@gmail.com>
2019-12-22 12:23:53 +0100

    changed appicon / removed boilerplate code from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'fitment',
-      theme: ThemeData(
-        primaryColor: Color(0xFF6B34FB),
-      ),
-      home: MainPage(title: 'fitment'),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  MainPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Image.asset(
+            'assets/appIcons/category.png',
+            height: 5.0,
+            width: 5.0,
+          ),
+          title: Text('fitment', style: TextStyle(color: Color(0xFF3C3C3C)), textAlign: TextAlign.center,),
+          backgroundColor: Color(0x88ECECEC),
+        ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Image(
+          image: NetworkImage('https://firebasestorage.googleapis.com/v0/b/fitment-mrcgrhrdt.appspot.com/o/category.png?alt=media&token=fc956428-f413-4f5c-b8fb-19687225de83'),
+          width: 10.0,
+          height: 10.0,
         ),
       ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-        backgroundColor: Color(0xFF6B34FB)
       ),
-    );
-  }
+    ),
+  );
 }
