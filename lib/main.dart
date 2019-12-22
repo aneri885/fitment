4677892edd7272fe0f002def4054d28901a562b0
Maurice Gerhardt <mrcgrhrdt@gmail.com>
2019-12-22 13:26:24 +0100

    removed old category icon / restructured main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: Image.asset(
-            'assets/appIcons/category.png',
-            height: 5.0,
-            width: 5.0,
+          title: Container(
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage('assets/appIcons/category_accent.png'),
+                  width: 32.0,
+                  height: 32.0,
+                  fit: BoxFit.fitHeight,
+                ),
+                SizedBox(width: 16.0),
+                Text('fitment', style: TextStyle(color: Color(0xFF3C3C3C)))
+              ],
+              
+            ),
           ),
-          title: Text('fitment', style: TextStyle(color: Color(0xFF3C3C3C)), textAlign: TextAlign.center,),
           backgroundColor: Color(0x88ECECEC),
-        ),
-      body: Center(
-        child: Image(
-          image: NetworkImage('https://firebasestorage.googleapis.com/v0/b/fitment-mrcgrhrdt.appspot.com/o/category.png?alt=media&token=fc956428-f413-4f5c-b8fb-19687225de83'),
-          width: 10.0,
-          height: 10.0,
+          elevation: 0.0,
         ),
       ),
-      ),
-    ),
-  );
+    );
+  }
 }
