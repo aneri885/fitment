53b1ccf10943d7c4da1a0516322c38174ed7f70a
Maurice <mrcgrhrdt@gmail.com>
2020-01-17 16:03:56 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 import 'package:fitment/main.dart';
 
 void main() {
